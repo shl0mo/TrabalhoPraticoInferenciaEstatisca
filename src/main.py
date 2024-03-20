1cb81787ac73d0bd7631dc65047005a72ee1a942
Matheus Serrao Uchoa-MSUCHOA1 <msuchoa1@motorola.com>
2024-03-20 09:04:22 -0400

    update

--- src/main.py
+++ src/main.py
@@ -4,24 +4,18 @@ import scipy.stats as stats
 import os
 import requests
 import zipfile
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 import streamlit as st
 
 from sklearn.linear_model import LinearRegression
 
-def getTestDone(data, var1, var2):
+def getTestDone(data):
     # Preliminary checks and setup
     if not isinstance(data, pd.DataFrame):
         data = pd.DataFrame(data)
     st.session_state['data'] = data
-    st.session_state['var1'] = var1
-    st.session_state['var2'] = var2
-
-    # Determine variable types
-    is_var1_categorical = pd.api.types.is_categorical_dtype(data[var1]) or pd.api.types.is_object_dtype(data[var1])
-    is_var2_categorical = pd.api.types.is_categorical_dtype(data[var2]) or pd.api.types.is_object_dtype(data[var2])
-
+   
     tests_options = []
     # Scenario handling based on variable types
     
@@ -31,35 +25,61 @@ def getTestDone(data, var1, var2):
     tests_options.append("Correlation Analysis")
     tests_options.append("Linear Regression Analysis")
     
-
     selected_test = st.selectbox("Select a test based on the variable types:", tests_options)
-
-    
-    print(tests_options)
     
     # Execute the selected test
     if selected_test == "Chi-square Test":
-        
-        results = chi_square_test(data)
+        var1,var2= select_colums_categorico(data)
+        chi_square_value, p_value, dof, expected = chi_square_test(data,var1,var2)
+        print("Chi-square Test Results")
+        print( p_value)
         st.write("Chi-square Test Results")
-        st.write(results)
+        st.write(f"Chi-square Value: {chi_square_value}")
+        st.write(f"P-value: {p_value}")
+        st.write(f"Degrees of Freedom: {dof}")
+
+        # Formatar os valores esperados (expected) como um DataFrame para exibição
+        expected_df = pd.DataFrame(expected, columns=[f"Var{i+1}" for i in range(expected.shape[1])])
+        st.write("Expected Frequencies:")
+        st.dataframe(expected_df)
+
         
     elif selected_test == "T-test":
+        select_colums_categorico(data)
         results = student_t_test(data)
         display_test_results("T-test Results", results)
         
     elif selected_test == "ANOVA":
+        select_colums_categorico(data)
         results = anova_test(data)
         display_test_results("ANOVA Results", results)
-        
-    elif selected_test == "Correlation Analysis":
-        st.write("Both variables are continuous. Performing Correlation Analysis...")
+    
+    if selected_test == "Correlation Analysis":    
+        var1, var2 = select_columns_continuo(data)
         results = correlation_analysis(data, var1, var2)
         display_test_results("Correlation Analysis Results", results)
     elif selected_test == "Linear Regression Analysis":
-        st.write("Both variables are continuous. Performing Linear Regression Analysis...")
+        var1, var2 = select_columns_continuo(data)
         results = linear_regression_analysis(data, var1, var2)
         display_test_results("Linear Regression Analysis Results", results)
+        
+def select_columns_continuo(data):
+    # verify cada coluna do dataset se é numérica ou categórica
+    available_columns = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
+    var1 = st.selectbox('Select the first variable for comparison:', available_columns, index=0)
+    var2 = st.selectbox('Select the second variable for comparison:', available_columns, index=min(1, len(available_columns)-1))
+    st.session_state['var1'] = var1
+    st.session_state['var2'] = var2
+    return var1, var2
+
+def select_colums_categorico(data):
+    
+    available_columns = data.select_dtypes(include=['object', 'category']).columns.tolist()
+    var1 = st.selectbox('Select the first variable for comparison:', available_columns, index=0)
+    var2 = st.selectbox('Select the second variable for comparison:', available_columns, index=min(1, len(available_columns)-1))
+    st.session_state['var1'] = var1
+    st.session_state['var2'] = var2
+    return var1, var2
 
 def display_test_results(title, results):
     st.subheader(title)
@@ -71,44 +91,69 @@ def display_test_results(title, results):
         for key, value in results.items():
             st.metric(label=key, value=f"{value:.3f}")
 
-def chi_square_test(data, alpha=0.05):
-    chi_square_statistic_all = {}
-    p_value_all = {}
-    # Assuming each column represents a categorical variable
-    for column in data.columns:
-        # Extract the feature data
-        feature = data[column]
-        # Calculate observed frequency counts
-        observed_freq = feature.value_counts().sort_index()
-        observed_values = observed_freq.values
-        # Calculate expected values assuming uniform distribution
-        num_categories = len(observed_freq)
-        expected_values = np.full(num_categories, observed_values.sum() / num_categories)
-        # Perform chi-square test
-        chi_square_statistic, p_value = stats.chisquare(f_obs=observed_values, f_exp=expected_values)
+def chi_square_test(data, var1, var2):
+    # Create contingency table
+    contingency_table = pd.crosstab(data[var1], data[var2])
+    
+    # Convert index and columns to string for compatibility
+    contingency_table.index = contingency_table.index.map(str)
+    contingency_table.columns = contingency_table.columns.map(str)
+    
+    # Display the contingency table
+    print("Contingency Table:")
+    print(contingency_table)
+    
+   
+    contingency_table_df = pd.DataFrame(contingency_table).reset_index()
+   
 
-        # Print test results
-        print(f"Chi-Square Test for the variable '{column}':")
-        print(f"Test Statistic: {chi_square_statistic}")
-        print(f"P-value: {p_value}")
-        if p_value < alpha:
-            print("Null hypothesis rejected - The distribution is not uniform.")
-        else:
-            print("Failed to reject the null hypothesis - The distribution is uniform.")
-        print("\n")
+    # Convertendo a tabela de contingência para HTML
+    contingency_table_html = contingency_table.to_html()
+    
+    # Usando st.markdown para exibir o HTML com a opção unsafe_allow_html ativada
+    st.markdown("Contingency Table:", unsafe_allow_html=True)
+    st.markdown(contingency_table_html, unsafe_allow_html=True)
+    
+    # Perform the chi-square test
+    chi2, p, dof, expected = stats.chi2_contingency(contingency_table)
+    return chi2, p, dof, expected
+
+def generateVisualizationsInTabs(data):
+    available_columns = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
+    
+    with st.container():
+        # Abas principais para Histogramas e Box Plots
+        tab_histograms, tab_boxplots = st.tabs(["Histograms", "Box Plots"])
         
-        chi_square_statistic_all[column] = chi_square_statistic
-        p_value_all[column] = p_value
+        # Geração de Histogramas em uma aba principal
+        with tab_histograms:
+            for column in available_columns:
+               
+                # Usando st.expander para tornar cada histograma colapsável
+                with st.expander(f"Histogram for {column}"):
+                    fig, ax = plt.subplots()
+                    ax.hist(data[column])
+                    print("data column test")
+                    print(data[column].head())
+                    ax.set_title(f"Histogram for {column}")
+                    st.pyplot(fig)
         
-    return {"Chi-Square Statistic": chi_square_statistic_all, "P-value": p_value_all} 
+        # Geração de Box Plots em outra aba principal
+        with tab_boxplots:
+            for column in available_columns:
+                # Usando st.expander para tornar cada box plot colapsável
+                with st.expander(f"Boxplot for {column}"):
+                    fig, ax = plt.subplots()
+                    ax.boxplot(data[column], vert=False)
+                    ax.set_title(f"Boxplot for {column}")
+                    st.pyplot(fig)      
 
 def student_t_test(data):
     #rafael
     print("ok")
     # Retorna os resultados dos testes
+    test_results={}
     return test_results
-
-
 
 def anova_test(data, continuous_var, categorical_var, alpha=0.05):
 
@@ -129,8 +174,7 @@ def anova_test(data, continuous_var, categorical_var, alpha=0.05):
         stat, p_value = stats.f_oneway(*samples)
         results = [stat, p_value]
         display_test_results("ANOVA Results", results)
-        
-        
+              
 def identify_variables(data):
     num_vars = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
     cat_vars = data.select_dtypes(include=['object', 'category']).columns.tolist()
@@ -143,7 +187,6 @@ def identify_variables(data):
         categorical_var = cat_vars[0]
 
     return categorical_var, continuous_var
-
 
 def correlation_analysis(data, var1, var2):
 
@@ -188,9 +231,6 @@ def linear_regression_analysis(data, independent_var, dependent_var):
     st.write("Higher R-squared values indicate a better fit for the model.")
 
     return [model.coef_[0], model.intercept_, model.score(X, y)]
-
-
-
 
 def download_dataset(dataset_url):
     # Check if the dataset exists
@@ -271,17 +311,7 @@ def unzip_dataset(path_to_zip_file, directory_to_extract_to,dataset_name):
     # dataset path name_of_the_data_set.data
     return os.path.join(directory_to_extract_to)
 
-
 def load_dataset(file_path):
-    """
-    Load the dataset based on its extension.
-    
-    Parameters:
-    - file_path: str, the path to the dataset file.
-    
-    Returns:
-    - A pandas DataFrame containing the loaded dataset.
-    """
     if file_path.endswith('.csv') or file_path.endswith('.data'):
         data = pd.read_csv(file_path, header=None)
     elif file_path.endswith('.xlsx'):
@@ -291,72 +321,12 @@ def load_dataset(file_path):
     
     return data
 
-def add_column_names(data):
-    """
-    Adds column names based on the length of the dataset.
-    
-    Parameters:
-    - data: DataFrame, the dataset without column names.
-    
-    Returns:
-    - The dataset with added column names.
-    """
-    data.columns = [f'feature_{i+1}' for i in range(data.shape[1])]
-    return data
-
-def display_dataset_info(data):
-    """
-    Display various information about the dataset.
-    
-    Parameters:
-    - data: DataFrame, the dataset to display information for.
-    """
-    print(data.head())  # Display the first 5 rows
-    print(data.shape)  # Display the shape
-    print(data.columns)  # Display the column names
-    print(data.dtypes)  # Display the data types
-    print(data.isnull().sum())  # Display missing values
-    print(data.describe())  # Display summary statistics
-
 def get_dataset_info(dataset_path):
-    """
-    Load a dataset and display its information.
-    
-    Parameters:
-    - dataset_path: str, the path to the dataset file.
-    
-    Returns:
-    - Various details about the dataset for further use.
-    """
+
     data = load_dataset(dataset_path)
-    data = add_column_names(data)
-    display_dataset_info(data)
+    
     
     return data.head(), data.shape, data.columns, data.dtypes, data.isnull().sum(), data.describe(), data
-
-        
-    if dataset_path_complete.endswith('.xlsx'):
-        # Load the dataset
-        data = pd.read_excel(dataset_path_complete, header=None)
-        # add column names based on length of the dataset
-        data.columns = [f'feature_{i+1}' for i in range(data.shape[1])]
-
-        # Display the first 5 rows of the dataset
-        print(data.head())
-        # Display the shape of the dataset
-        print(data.shape)
-        # Display the column names
-        print(data.columns)
-        # Display the data types of the columns
-        print(data.dtypes)
-        # Display the number of missing values in each column
-        print(data.isnull().sum())
-        # Display the summary statistics of the dataset
-        print(data.describe())
-    
-
-    return data.head(), data.shape, data.columns, data.dtypes, data.isnull().sum(), data.describe() , data
-
 
 def getDataSetFromPath(dataset_path):
     # Lista para armazenar os caminhos completos dos arquivos .data
@@ -378,13 +348,11 @@ def getDataSetFromPath(dataset_path):
             
     return datafiles_path
 
-
-
 def app():
     st.title('📊 Análise de Datasets')
 
     # Input do usuário para o link do dataset
-    dataset_url = st.text_input('🔗 Insira o link do dataset UCI', 'https://archive.ics.uci.edu/dataset/53/iris')
+    dataset_url = st.text_input('🔗 Insira o link do dataset UCI', 'https://archive.ics.uci.edu/dataset/2/adult')
     
     if st.button('📥 Baixar Dataset'):
         with st.spinner('Baixando o dataset...'):
@@ -409,26 +377,7 @@ def app():
         selected_file = st.selectbox('Selecione o arquivo do dataset', all_data)
         # Load the dataset
         data_head, data_shape, data_columns, data_dtypes, data_missing_values, data_description , selected_dataset = get_dataset_info(selected_file)
-
-        # Let the user select two variables from the dataset
-        if selected_dataset is not None and not selected_dataset.empty:
-            available_columns = selected_dataset.columns.tolist()
-            var1 = st.selectbox('Select the first variable for comparison:', available_columns, index=0)
-            var2 = st.selectbox('Select the second variable for comparison:', available_columns, index=min(1, len(available_columns)-1))
-            
-            # Assuming 'selected_file' is the file selected by the user
-            if 'dataset_path_complete' in st.session_state :
-
-                # Call the modified function with the selected dataset
-                getTestDone(selected_dataset, var1, var2)
-
-                # Display var information
-                st.write('Informações sobre as variáveis selecionadas:')
-                st.write(f'Variável 1: {var1}')
-                st.write(f'Variável 2: {var2}')
-                
-
-
+        
         if st.checkbox('🔍 Visualizar informações do dataset'):
             # Unpack the returned values from get_dataset_info
             
@@ -480,9 +429,16 @@ def app():
                 - **Valores Ausentes**: Visão dos dados ausentes que podem requerer tratamento.
                 - **Estatísticas**: Sumário estatístico do dataset para uma análise inicial.
                 """)
-
-   
-
-
+                
+        # Let the user select two variables from the dataset
+        if selected_dataset is not None and not selected_dataset.empty:
+            # show histogram and boxplot
+            generateVisualizationsInTabs(selected_dataset)
+            
+            # Assuming 'selected_file' is the file selected by the user
+            if 'dataset_path_complete' in st.session_state :
+                # Call the modified function with the selected dataset
+                getTestDone(selected_dataset)
+                
 if __name__ == "__main__":
     app()
